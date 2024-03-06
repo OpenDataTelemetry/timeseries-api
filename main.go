aed1fa8066231ed6942d7746953f0e368835e057
SocioPJ <sociopj45@outlook.com>
2024-03-06 14:59:30 -0300

    feat: :sparkles: Created functions

--- main.go
+++ main.go
@@ -2,21 +2,25 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"net/http"
-	"os"
-
+	// "fmt"
 	"github.com/InfluxCommunity/influxdb3-go/influxdb3"
 	"github.com/gin-gonic/gin"
+	"net/http"
+	"os"
+	// "reflect"
 )
 
-func main() {
-	// Use env variables to initialize client
+//	func getSmartLightbyID(c *gin.Context, objs map[string]interface{}){
+//		nodename := c.Param("nodename")
+var influxdb3Client *influxdb3.Client
+
+func getAllSmartLights(c *gin.Context) {
+
+	var objs = []gin.H{}
 	url := os.Getenv("INFLUXDB_URL")
 	token := os.Getenv("INFLUXDB_TOKEN")
 	database := os.Getenv("INFLUXDB_DATABASE")
 
-	// Create a new client using an InfluxDB server base URL and an authentication token
 	influxdb3Client, err := influxdb3.New(influxdb3.ClientConfig{
 		Host:     url,
 		Token:    token,
@@ -24,50 +28,82 @@ func main() {
 	})
 
 	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	defer influxdb3Client.Close()
+	query := `
+SELECT *
+FROM "SmartLights"
+ORDER BY time DESC;
+`
+
+	iterator, err := influxdb3Client.Query(context.Background(), query)
+
+	if err != nil {
 		panic(err)
 	}
 
-	// Close influxdb3Client at the end and escalate error if present
-	defer func(influxdb3Client *influxdb3.Client) {
-		err := influxdb3Client.Close()
-		if err != nil {
-			panic(err)
-		}
-	}(influxdb3Client)
+	for iterator.Next() {
+		value := iterator.Value()
+		obj := gin.H(value)
+		objs = append(objs, obj)
+	}
+
+	c.IndentedJSON(http.StatusOK, objs)
+
+}
+
+func getSmartLightbyID(c *gin.Context) {
+	nodename := c.Param("nodename")
+	var objs = []gin.H{}
+	url := os.Getenv("INFLUXDB_URL")
+	token := os.Getenv("INFLUXDB_TOKEN")
+	database := os.Getenv("INFLUXDB_DATABASE")
+
+	influxdb3Client, err := influxdb3.New(influxdb3.ClientConfig{
+		Host:     url,
+		Token:    token,
+		Database: database,
+	})
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	defer influxdb3Client.Close()
+	query := `
+SELECT *
+FROM "SmartLights"
+WHERE "nodename" = '` + nodename + `'
+ORDER BY time DESC;
+`
+	iterator, err := influxdb3Client.Query(context.Background(), query)
+
+	if err != nil {
+		panic(err)
+	}
+
+	for iterator.Next() {
+		value := iterator.Value()
+		obj := gin.H(value)
+		objs = append(objs, obj)
+	}
+
+	c.IndentedJSON(http.StatusOK, objs)
+}
+
+func main() {
 
 	// API
 	r := gin.Default()
-	r.GET("/api/v0.1/smartcampusmaua/SmartLights", func(c *gin.Context) {
+	api := r.Group("/api/v0.1/smartcampusmaua")
+	{
+		api.GET("/SmartLights", getAllSmartLights)
+		// api.GET("/SmartLights/:nodename", getSmartLightbyID)
 
-		// Prepare FlightSQL query
-		query := `
-		SELECT *
-		FROM "SmartLights"
-		WHERE
-		time >= now() - interval '1 hour'
-		AND
-		("data_ad" IS NOT NULL OR "data_boardVoltage" IS NOT NULL)
-		ORDER BY time DESC;
-	`
-		
-		iterator, err := influxdb3Client.Query(context.Background(), query)
-		fmt.Printf("\niterator: %v\n", iterator)
+	}
 
-		if err != nil {
-			panic(err)
-		}
-		
-		
-		objs := []gin.H{}
-		
-		for iterator.Next() {
-			value := iterator.Value()
-			obj := gin.H(value)
-			objs = append(objs, obj)
-		}
-		c.JSON(http.StatusOK, objs)
-
-	})
+	// router.GET("/SmartLights",)
 	r.Run(":8888")
 }
-
